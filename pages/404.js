e161e1ddfd247cc1168b092ca959d675e669dfb8
pRob3 <skene@hotmail.com>
2022-11-25 08:00:33 +0100

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, there is nothing to see here.</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
