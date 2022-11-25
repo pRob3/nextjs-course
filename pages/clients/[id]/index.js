24597d681ab936343e810d2d385e9e73164fbd9a
pRob3 <skene@hotmail.com>
2022-11-25 07:53:44 +0100

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,19 @@
+import { useRouter } from 'next/router';
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // send a request to the backend...
+
+    // navigate programmatically
+    router.push('/clients/max/projecta');
+  }
+
   return (
     <div>
       <h1>Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
